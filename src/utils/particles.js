b09a261d02a37a7eaebb0816e424ac93c1dcb9e2
Rahul Kumar <rahul.kumar@5datainc.com>
2024-05-23 22:37:29 +0530

    portfolio final commit

--- src/utils/particles.js
+++ src/utils/particles.js
@@ -1,69 +1,71 @@
-export default{
-        background: {
-            color: {
-                value: "#1d1d1d",
-            },
+const particlesConfig = {
+    background: {
+        color: {
+            value: "#1d1d1d",
         },
-        fpsLimit: 120,
-        interactivity: {
-            events: {
-                onClick: {
-                    enable: true,
-                    mode: "push",
-                },
-                onHover: {
-                    enable: true,
-                    mode: "repulse",
-                },
-                resize: true,
-            },
-            modes: {
-                push: {
-                    quantity: 4,
-                },
-                repulse: {
-                    distance: 200,
-                    duration: 0.4,
-                },
-            },
-        },
-        particles: {
-            color: {
-                value: "#ffffff",
-            },
-            links: {
-                color: "#ffffff",
-                distance: 150,
+    },
+    fpsLimit: 120,
+    interactivity: {
+        events: {
+            onClick: {
                 enable: true,
-                opacity: 0.5,
-                width: 1,
+                mode: "push",
             },
-            move: {
-                direction: "none",
+            onHover: {
                 enable: true,
-                outModes: {
-                    default: "bounce",
-                },
-                random: false,
-                speed: 6,
-                straight: false,
+                mode: "repulse",
             },
-            number: {
-                density: {
-                    enable: true,
-                    area: 800,
-                },
-                value: 80,
+            resize: true,
+        },
+        modes: {
+            push: {
+                quantity: 4,
             },
-            opacity: {
-                value: 0.5,
-            },
-            shape: {
-                type: "circle",
-            },
-            size: {
-                value: { min: 1, max: 5 },
+            repulse: {
+                distance: 200,
+                duration: 0.4,
             },
         },
-        detectRetina: true,
-    };
+    },
+    particles: {
+        color: {
+            value: "#ffffff",
+        },
+        links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+        },
+        move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+                default: "bounce",
+            },
+            random: false,
+            speed: 6,
+            straight: false,
+        },
+        number: {
+            density: {
+                enable: true,
+                area: 800,
+            },
+            value: 80,
+        },
+        opacity: {
+            value: 0.5,
+        },
+        shape: {
+            type: "circle",
+        },
+        size: {
+            value: { min: 1, max: 5 },
+        },
+    },
+    detectRetina: true,
+};
+
+export default particlesConfig;
